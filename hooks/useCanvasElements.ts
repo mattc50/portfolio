492c11538408d3c05ff20765f2ece08a206fab17
Matthew Canabarro <67874965+mattc50@users.noreply.github.com>
2026-04-12 16:51:44 -0400

    testing code omit

--- hooks/useCanvasElements.ts
+++ hooks/useCanvasElements.ts
@@ -139,9 +139,9 @@ export function useCanvasElements(
       isDraggingId.current = null; // 👈 clear on release
       socket?.send(JSON.stringify({ type: "rect:drag-end", id: element.id }));
 
-      if (e.pointerType === "touch") {
-      socket?.send(JSON.stringify({ type: "leave" }));
-    }
+      // if (e.pointerType === "touch") {
+      //   socket?.send(JSON.stringify({ type: "leave" }));
+      // }
     },
     [socket]
   );
