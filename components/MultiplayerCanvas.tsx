993f6924011e575f7e1b725a5d90a839ee193728
Matthew Canabarro <67874965+mattc50@users.noreply.github.com>
2026-04-12 17:06:18 -0400

    changes 2

--- components/MultiplayerCanvas.tsx
+++ components/MultiplayerCanvas.tsx
@@ -137,7 +137,6 @@ export function MultiplayerCanvas({ onRectClick }: Props) {
           position: "relative",
           backgroundColor: "var(--canvas)",
           cursor: spaceHeld ? "grab" : "default",
-          touchAction: "none",
         }}
         // onWheel={(e) => {
         //   const rect = containerRef.current?.getBoundingClientRect();
@@ -165,6 +164,7 @@ export function MultiplayerCanvas({ onRectClick }: Props) {
             height: 500,
             transform: `translate(${canvasTransform.transform.x}px, ${canvasTransform.transform.y}px) scale(${canvasTransform.transform.scale})`,
             transformOrigin: "0 0",
+            touchAction: "none",
           }}
         >
           {/* Elements layer */}
